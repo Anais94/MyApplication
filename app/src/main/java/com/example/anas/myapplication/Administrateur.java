d578f0f53b6d1d1dadf543c75a49dd7d18a0a214
Anaïs <allaert.anais@gmail.com>
2015-03-20 20:42:58 +0100

    dxeme commit ac changmtb

--- app/src/main/java/com/example/anas/myapplication/Administrateur.java
+++ app/src/main/java/com/example/anas/myapplication/Administrateur.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class Administrateur extends ActionBarActivity {
-    gdfta
+    gdftab
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
